c4a90dbaeec8fb3978326924a266c7c5594cc81d
tobium <tobi@ultramixer.com>
2017-03-06 10:47:48 +0100

    various

---
+++ src/main/java/com/ultramixer/javafxperformancetest/TransparentNode.java
@@ -0,0 +1,13 @@
+package com.ultramixer.javafxperformancetest;
+
+import javax.swing.plaf.synth.Region;
+
+/**
+ * Created by tobi on 03.03.2017.
+ */
+public class TransparentNode extends javafx.scene.layout.Region {
+   public TransparentNode()
+   {
+       this.setStyle("-fx-background-color: red;");
+   }
+}
